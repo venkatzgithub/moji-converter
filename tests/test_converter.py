24ef9f5574e47b2af65a31e85e683cfc08abdbf9
venkatzgithub <imvenkatprasad@gmail.com>
2021-03-24 23:35:04 +0900

    test.py

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -1,8 +1,13 @@
 import pytest
-from mojiconverter.app import katakana
+from mojiconverter.app import katakana,hiragana
 
 
 def test_katakana():
     """ Test that checks the conversion of hiragana to katakana"""
     input_str = "こんにちは"
-    assert katakana(input_str) == "こんにちは"
+    assert katakana(input_str) == "コンニチハ"
+
+def test_hiragana():
+    """ Test that checks the conversion of katakana to hiragana"""
+    input_str = "コンニチハ"
+    assert hiragana(input_str) == "こんにちは"
